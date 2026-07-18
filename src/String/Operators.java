c8c40aed67a64950c743d60c764f1048eb97bf7d
Akshada Bide <akshadabide296@gmail.com>
2026-07-18 15:14:53 +0530

    Operator_Overloading in String

--- src/String/Operators.java
+++ src/String/Operators.java
@@ -8,7 +8,7 @@ public class Operators {
         System.out.println('a' + 'b');  //add asci value
         System.out.println("a" + "b");  //ab
         System.out.println('a' + 3); //100
-        System.out.println((char)('a' + 3)); //d
+        System.out.println((char)('a' + 3)); //d 
         System.out.println("a" + 1);  //a1
         //integer will be converted to Integer that will call toString()
         System.out.println("a"+ 'b'); //ab :- if one of the datatype is string output will be string
