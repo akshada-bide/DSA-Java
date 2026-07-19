e9494296040b19cbb7dd49f902cee55826ab84a7
Akshada Bide <akshadabide296@gmail.com>
2026-07-19 19:08:29 +0530

    PrettyPrinting

--- src/String/PrettyPrinting.java
+++ src/String/PrettyPrinting.java
@@ -7,6 +7,7 @@ public class PrettyPrinting {
         System.out.println();
         System.out.printf("pie: %.3f",Math.PI);
         System.out.println();
+        //
         System.out.printf("Hello My name is %s & I am %s", "kunal", "cool");
 
     }
