833b60d1ad625d880dea2553b882978ade314761
Akshada Bide <akshadabide296@gmail.com>
2026-07-23 23:50:28 +0530

    DigitSum

--- src/Maths/DigitSum.java
+++ src/Maths/DigitSum.java
@@ -8,7 +8,7 @@ public class DigitSum {
     }
     static int alternateDigitSum(int n){
         String s = Integer.toString(n);
-        int sum = 0;
+        int sum = 0; 
         
         for(int i=0; i<s.length(); i++){
             int digit = s.charAt(i) - '0';
