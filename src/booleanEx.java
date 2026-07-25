5d7609a6dd9806095a4a40e65fdce10d0a0118e0
Akshada Bide <akshadabide296@gmail.com>
2026-07-25 22:29:37 +0530

    Boolean

--- src/booleanEx.java
+++ src/booleanEx.java
@@ -9,7 +9,6 @@ public class booleanEx {
         if(arr.length == 0){
             return false;
         }
-        
         for(int element:arr){
             if(element == target){
                 return true;
