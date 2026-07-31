8bdbf821f988f05b9ad0421d2e96ae4e7c906898
Akshada Bide <akshadabide296@gmail.com>
2026-07-31 19:34:06 +0530

    LeetCode 2544: Alternating digit sum

--- src/Maths/DigitSum.java
+++ src/Maths/DigitSum.java
@@ -11,6 +11,7 @@ public class DigitSum {
         int sum = 0; 
         
         for(int i=0; i<s.length(); i++){
+            
             int digit = s.charAt(i) - '0';
 
             if(i % 2 == 0){
