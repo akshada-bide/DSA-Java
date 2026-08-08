fd8d014a3f7708ccd9eac71f5e0d033ee9498794
Akshada <akshadabide296@gmail.com>
2026-08-08 16:48:22 +0530

    Minimum Number Of Pushes to Type Word I

---
+++ src/String/LeetCode3014.java
@@ -0,0 +1,17 @@
+package String;
+
+//LeetCode 3014: Minimum Number Of Pushes to Type Word I
+public class LeetCode3014 {
+    public static void main(String[] args){
+        String word ="abcde";
+        System.out.println(minimumPushes(word));
+    }
+    static int minimumPushes(String word) {
+        int ans=0;
+        for(int i=0; i<word.length(); i++){
+            ans += (i/8) + 1;
+        }
+        return ans;
+
+    }
+}
