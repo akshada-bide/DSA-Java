a5468e40312ea737d44f953023a7a975e0c16706
Akshada <akshadabide296@gmail.com>
2026-08-09 21:31:35 +0530

    LeetCode 4014: Minimum Total Price after Applying Discounts

---
+++ src/WeeklyBiWeekly_Contets/Contest2.java
@@ -0,0 +1,30 @@
+package WeeklyBiWeekly_Contets;
+
+//LeetCode 4014: Minimum Total Price after Applying Discounts
+import java.util.Arrays;
+public class Contest2 {
+    public static void main(String[] args){
+        int[] prices = {10,30,21};
+        int[] discounts = {50,60};
+        System.out.println(minPrice(prices, discounts));
+
+    }
+    static double minPrice(int[] prices, int[] discounts) {
+        Arrays.sort(prices);
+        int i = prices.length - 1;
+        int j = discounts.length -1;
+
+        double total = 0;
+        while(i>=0 && j>=0){
+            total += prices[i]*(100.0 -discounts[j])/100.0;
+            i--;
+            j--;
+        }
+        while(i>=0){
+            total += prices[i];
+            i--;
+        }
+        return total;
+
+    }
+}
