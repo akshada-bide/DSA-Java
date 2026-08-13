28e4c8ec9dbd45f61b5867b2992fc9c2970da28b
Akshada Bide <akshadabide296@gmail.com>
2026-08-13 23:07:33 +0530

    Find Target

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     }
     static int search(int[] arr, int target){
         if(arr.length == 0){
-            return -1;
+            return -1; 
         }
         for (int i = 0; i <arr.length; i++) {
             int element = arr[i];
