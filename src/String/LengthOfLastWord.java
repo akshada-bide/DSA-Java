03e9dd123d973b210ea30db7235a5f59783d5925
Akshada Bide <akshadabide296@gmail.com>
2026-08-14 23:22:10 +0530

    LeetCode 58 : Length of Last Word

--- src/String/LengthOfLastWord.java
+++ src/String/LengthOfLastWord.java
@@ -4,7 +4,6 @@ public class LengthOfLastWord {
     public static void main(String[] args){
         String s = "Hello World";
         System.out.println(lengthOfLastWord(s));
-
     }
     static int lengthOfLastWord(String s){
         int count =0;
