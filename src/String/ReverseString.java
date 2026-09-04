8717d477d24defade70d8df5cf920c274c551ffd
Akshada <akshadabide296@gmail.com>
2026-09-04 23:47:24 +0530

    Reverse String ex

---
+++ src/String/ReverseString.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void main(String[] args){
+        String s = "Hello World";
+        isReverse(s);
+//        System.out.println((s));
+
+    }
+    //to reverse the word
+    static String isReverse(String s) {
+        String[] words = s.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+        return "";
+    }
+
+}
