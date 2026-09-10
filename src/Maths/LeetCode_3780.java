f699e6b0bf4bf0afb006b00ba7f7278e1a02f85b
Akshada <akshadabide296@gmail.com>
2026-09-10 18:55:25 +0530

    Leetcode 3780: Count Commas in Range

---
+++ src/Maths/LeetCode_3780.java
@@ -0,0 +1,18 @@
+package Maths;
+
+//Leetcode 3780: Count Commas in Range
+public class LeetCode_3780 {
+    public static void main(String[] args){
+        int n = 1002;
+        System.out.println(countCommas(n));
+    }
+    static int countCommas(int n){
+        int count =0;
+        for(int i=1; i<=n; ++i){
+            if(i>999){
+                count++;
+            }
+        }
+        return count;
+    }
+}
